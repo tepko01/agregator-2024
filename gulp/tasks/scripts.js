231be96fe7be589983a8a2a37e535cd0057ef625
vladok <vladokbln@gmail.com>
2024-03-19 17:06:16 +0100

    schools-slider начало слайдера

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -6,13 +6,19 @@ const uglify = require('gulp-uglify'),
 			};
 module.exports = function () {
 	$.gulp.task('libsJS:dev', () => {
-		return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+		return $.gulp.src([
+			'node_modules/svg4everybody/dist/svg4everybody.min.js',
+			'node_modules/slick-carousel/slick/slick.js'
+			])
 			.pipe(concat('libs.min.js'))
 			.pipe($.gulp.dest(scriptsPATH.output));
 	});
 
 	$.gulp.task('libsJS:build', () => {
-		return $.gulp.src(['node_modules/svg4everybody/dist/svg4everybody.min.js'])
+		return $.gulp.src([
+			'node_modules/svg4everybody/dist/svg4everybody.min.js',
+			'node_modules/slick-carousel/slick/slick.js'
+		])
 			.pipe(concat('libs.min.js'))
 			.pipe(uglify())
 			.pipe($.gulp.dest(scriptsPATH.output));
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
     svg4everybody({});
+
+		$('.schools-slider-js').slick({
+			infinite: true,
+			dots: true,
+			slidesToShow: 4,
+			prevArrow: '.schools-slider__nav--prev',
+			nextArrow: '.schools-slider__nav--next'
+		});
 });
